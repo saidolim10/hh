15e3d8dfd860aeb2784f9ec409b11c57e4c9d61d
saidolim10 <git001hub@gmail.com>
2024-11-21 16:03:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let names = prompt(`Ismingizmni kriting`)
+let id = prompt(`space idingizmni kriting`)
+let parol = prompt(`space parolingizmni kriting`)
+
+let obj = {
+    name: names,
+    id: id,
+    password: parol
+}
+
+console.log(obj);
